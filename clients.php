0ace1004206dc6197940379022e9ea4cba5f4464
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-03-05 19:27:03 +0000

    Finition de la liaison entre la table billet et le formulaire

--- clients.php
+++ clients.php
@@ -33,7 +33,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='utilisateur.css'>
     <title>Agence de voyage inscription</title>
 </head>
 <body>
